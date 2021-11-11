cf31643f9b04ffbdd670bec5f634a7a671281880
MikeJonckheere <mikejonckheere@hotmail.com>
2021-11-11 14:37:14 +0100

    Aanmaken van solution en folders + paar van de classes.

---
+++ Remote/Remote/RemoteProgram/Input.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Remote.RemoteProgram
+{
+    class Input //Alles voor de knoppen in het prog te kunnen laten voeren.
+    {
+    }
+}
---
+++ Remote/Remote/RemoteProgram/Output.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Remote.RemoteProgram
+{
+    class Output //Alles voor in de logging en errorhandeling.
+    {
+    }
+}
