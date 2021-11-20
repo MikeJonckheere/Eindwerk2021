f8dfc5831e32f260d4031fc7943391b07e6d04d8
DESKTOP-17GUQLP\fliefhooghe <fliefhooghe@gmail.com>
2021-11-20 08:26:50 +0100

    update repo remote

--- Remote/Repositories/sqlRepository.cs
+++ Remote/Repositories/sqlRepository.cs
@@ -8,30 +8,24 @@ namespace Remote.Repositories
 {
     class sqlRepository
     {
-        const string _connectionString = "Data Source=localhost; Initial Catalog=Remote; User ID=sa; Password=SQL12345";
+        const string _connectionString = "Data Source=localhost; Initial Catalog=Television; User ID=sa; Password=SQL12345";
 
         public List<RemoteControl> GetCurrentValues()
         {
             var result = new List<RemoteControl>();
 
-            string sql = "SELECT CurrentChannel FROM Channel SELECT @@IDENTITY";
-            string sql2 = "SELECT CurrentVolume FROM Volume SELECT @@IDENTITY";
-            string sql3 = "SELECT CurrentSource FROM Source SELECT @@IDENTITY";
+            string sql = "SELECT Channel, Volume, Source FROM TvCurrent SELECT @@IDENTITY";
 
             using (var connection = new SqlConnection(_connectionString))
             using (var command = new SqlCommand(sql, connection))
-            using (var command2 = new SqlCommand(sql2, connection))
-            using (var command3 = new SqlCommand(sql3, connection))
             {
                 connection.Open();
                 var reader = command.ExecuteReader();
                 reader.Read();
 
-                var channel = reader.GetInt32(reader.GetOrdinal("CurrentChannel"));
-                reader = command2.ExecuteReader();
-                var volume = reader.GetInt32(reader.GetOrdinal("CurrentVolume"));
-                reader = command3.ExecuteReader();
-                var source = reader.GetInt32(reader.GetOrdinal("CurrentSource"));
+                var channel = reader.GetInt32(reader.GetOrdinal("Channel"));
+                var volume = reader.GetInt32(reader.GetOrdinal("Volume"));
+                var source = reader.GetInt32(reader.GetOrdinal("Source"));
 
                 var remoteControl = new RemoteControl();
                 remoteControl.FillRemote(channel, volume, source);
